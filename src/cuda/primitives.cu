justinkb/CTranslate2-rocm

fixes for rocm7.0

src/cuda/primitives.cu
@@ -18,12 +18,12 @@
 #define CUBLAS_COMPUTE_32I HIPBLAS_COMPUTE_32I
 #define CUDA_R_32F HIP_R_32F
 #define CUDA_R_16BF HIP_R_16BF
-#define cublasGemmEx hipblasGemmEx_v2
+#define cublasGemmEx hipblasGemmEx_64
 #define CUDA_R_8I HIP_R_8I
 #define CUDA_R_32I HIP_R_32I
 #define CUBLAS_GEMM_DEFAULT_TENSOR_OP HIPBLAS_GEMM_DEFAULT
 #define cublasSgemmStridedBatched hipblasSgemmStridedBatched
-#define cublasGemmStridedBatchedEx hipblasGemmStridedBatchedEx_v2
+#define cublasGemmStridedBatchedEx hipblasGemmStridedBatchedEx_64
 #else
 #include <cuda_runtime.h>
 #include <cublas_v2.h>

sebastianmuell/ctranslate2

src/cuda/primitives.cu
@@ -18,12 +18,12 @@
 #define CUBLAS_COMPUTE_32I HIPBLAS_COMPUTE_32I
 #define CUDA_R_32F HIP_R_32F
 #define CUDA_R_16BF HIP_R_16BF
-#define cublasGemmEx hipblasGemmEx_v2
+#define cublasGemmEx hipblasGemmEx
 #define CUDA_R_8I HIP_R_8I
 #define CUDA_R_32I HIP_R_32I
 #define CUBLAS_GEMM_DEFAULT_TENSOR_OP HIPBLAS_GEMM_DEFAULT
 #define cublasSgemmStridedBatched hipblasSgemmStridedBatched
-#define cublasGemmStridedBatchedEx hipblasGemmStridedBatchedEx_v2
+#define cublasGemmStridedBatchedEx hipblasGemmStridedBatchedEx
 #else
 #include <cuda_runtime.h>
 #include <cublas_v2.h>